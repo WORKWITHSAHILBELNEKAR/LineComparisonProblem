39022c8017f4fc9bf355e24c22f88da3b9f0396f
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-14 17:41:39 +0530

    Line compare program started

---
+++ com/LineComparison/LineCompare.java
@@ -0,0 +1,7 @@
+package com.LineComparison;
+
+public class LineCompare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computational program");
+    }
+}
