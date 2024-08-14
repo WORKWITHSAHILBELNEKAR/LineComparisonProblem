c0d776feef784d17bfba60204b1bbd9568b4e4b7
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-08-14 17:53:07 +0530

    i calculated the length

--- com/LineComparison/LineCompare.java
+++ com/LineComparison/LineCompare.java
@@ -1,7 +1,28 @@
 package com.LineComparison;
 
+import java.util.Scanner;
+
 public class LineCompare {
+    static Scanner sc = new Scanner(System.in);
+    static double x1,y1,x2,y2;
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computational program");
+        System.out.println("Welcome to Line Comparision Computation Program!");
+        input();
+        double result =calculateLength();
+        System.out.println("Length of Line : "+ result);
     }
+
+    private static void input() {
+        System.out.println("Enter coordinates of the first point (x1, y1): ");
+        x1 = sc.nextDouble();
+        y1 = sc.nextDouble();
+        System.out.println("Enter coordinates of the first point (x1, y1): ");
+        x2 = sc.nextDouble();
+        y2 = sc.nextDouble();
+    }
+
+    private static double calculateLength() {
+        return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+    }
+
 }
